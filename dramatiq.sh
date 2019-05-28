95a803d18accb468ae5b73dabf23042a49d424d4
Selvakumar Subramanian <mydreams.info@gmail.com>
2019-05-29 02:36:26 +0530

    Bugfix: docker run error

--- dramatiq.sh
+++ dramatiq.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 dramatiq geolocation/main.py
  
--- tests/test_geolocation.py
+++ tests/test_geolocation.py
@@ -4,6 +4,7 @@ import geolocation
 import geocoder
 from dramatiq import Message, Middleware
 
+
 def test_actor_geo_location(stub_broker):
     @dramatiq.actor
     def geo_location(location):
@@ -11,17 +12,20 @@ def test_actor_geo_location(stub_broker):
     # assert isinstance(geo_location("Buddingevej 152800 Kongens Lyngby"), dramatiq.Actor)
     assert geo_location.actor_name == "geo_location"
 
+
 def test_actor_geo_location_address(stub_broker):
     @dramatiq.actor
     def geo_location(location):
         return geocoder.osm(location).latlng
     assert geo_location("Buddingevej 152800 Kongens Lyngby") == [55.7673935, 12.4980629]
 
+
 def test_actor_geo_location_coordinates(stub_broker):
     @dramatiq.actor
     def geo_location(location):
         return geocoder.osm(location, method="reverse").street
     assert geo_location("55.674146, 12.569553") == "Tietgensgade"
+
 
 def test_actor_geo_location_message(stub_broker):
     @dramatiq.actor
@@ -31,6 +35,7 @@ def test_actor_geo_location_message(stub_broker):
     _message = geo_location.send("55.674146, 12.569553")
     _message_data = stub_broker.queues["default"].get(timeout=1)
     assert _message == Message.decode(_message_data)
+
 
 def test_geo_location_success_callbacks(stub_broker, stub_worker):
     @dramatiq.actor
